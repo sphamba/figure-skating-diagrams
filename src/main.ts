b96dc02fa9a7b30cf461611db42f44b6024cb477
Son Pham-Ba <son.phamba@epfl.ch>
2026-09-08 09:46:37 +0200

    feat: use light blue (sky) primary color

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,28 @@ import { createPinia } from "pinia";
 import OpenVue from "openvue/config";
 import Ripple from "openvue/ripple";
 import Aura from "@openvue/themes/aura";
+import { definePreset } from "@openuxkit/themes";
 
 import App from "./App.vue";
 import router from "./router";
+
+const appPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "{sky.50}",
+      100: "{sky.100}",
+      200: "{sky.200}",
+      300: "{sky.300}",
+      400: "{sky.400}",
+      500: "{sky.500}",
+      600: "{sky.600}",
+      700: "{sky.700}",
+      800: "{sky.800}",
+      900: "{sky.900}",
+      950: "{sky.950}",
+    },
+  },
+});
 
 const app = createApp(App);
 
@@ -17,7 +36,7 @@ app.use(router);
 app.directive("ripple", Ripple);
 app.use(OpenVue, {
   theme: {
-    preset: Aura,
+    preset: appPreset,
     options: {
       prefix: "p",
       darkModeSelector: "system",
